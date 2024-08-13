22fd7a66b6844efd9c7f72e7966a47ef13875aa4
epYuri <epoyri1@gmail.com>
2024-08-13 20:36:56 +0800

    Add files via upload

---
+++ js/zhuYeJs.js
@@ -0,0 +1,37 @@
+const vm = new Vue({
+    el: '#guanXi',
+    data() {
+        return {
+            name: 'Yuri',
+            touXiang: 'img/touXiang/Yuri.png',
+            lianJie: 'https://space.bilibili.com/229041529?spm_id_from=333.1007.0.0',
+            pengYou: [
+                {
+                    name: '01',
+                    touXiang: 'img/touxiang/weiJieSuo.png',
+                    lianJie: '#',
+                },
+                {
+                    name: '02',
+                    touXiang: 'img/touxiang/weiJieSuo.png',
+                    lianJie: '#',
+                },
+                {
+                    name: '03',
+                    touXiang: 'img/touxiang/weiJieSuo.png',
+                    lianJie: '#',
+                },
+                {
+                    name: '04',
+                    touXiang: 'img/touxiang/weiJieSuo.png',
+                    lianJie: '#',
+                },
+                {
+                    name: '05',
+                    touXiang: 'img/touxiang/weiJieSuo.png',
+                    lianJie: '#',
+                },
+            ],
+        };
+    },
+});
